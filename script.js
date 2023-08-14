41741e5576f7a6de5712a2f60f9c2fb334418ab6
KusumaMurthy109 <116034874+KusumaMurthy109@users.noreply.github.com>
2023-08-14 12:32:13 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,93 @@
+const questions = [
+    {
+        question: "Which is the largest animal",
+        answers: [
+            { text: "Shark", correct: false},
+            { text: "blue whale", correct: true},
+            { text: "elephant", correct: false},
+            { text: "giraffe", correct: false},
+        ]
+    },
 
-const btnNavEl = document.querySelector(".btn btn--full margin-right-sm")
+    {
+        question: "Which is the smallest contry",
+        answers: [
+            { text: "Shark", correct: false},
+            { text: "vatican", correct: true},
+            { text: "elephant", correct: false},
+            { text: "giraffe", correct: false},
+        ]
+    },
 
+    {
+        question: "click green",
+        answers: [
+            { text: "green", correct: true},
+            { text: "blue whale", correct: false},
+            { text: "elephant", correct: false},
+            { text: "giraffe", correct: false},
+        ]
+    },
 
+    {
+        question: "click yellow",
+        answers: [
+            { text: "green", correct: false},
+            { text: "blue whale", correct: false},
+            { text: "yellow", correct: true},
+            { text: "giraffe", correct: false},
+        ]
+    }
 
+];
 
-function checkFlexGap(){
-    var flex = document.createElement("div");
-    flex.style.display = "flex";
-    flex.style.flexDirection = "Column";
-    flex.style.rowGap = "1px";
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
 
-    flex.appendChild(document.createElement("div"));
-    flex.appendChild(document.createElement("div"));
+let currentQuestionIndex = 0;
+let score = 0;
 
-    document.body.appendChild(flex);
-    var isSupported = flex.scrollHeight ===1;
-    flex.parentNode.removeChild(flex);
-    console.log(isSupported);
-
-
-    if(!isSupported) document.body.classList.add("no-flexbox-gap");
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
 }
 
-checkFlexGap();
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+        if(answerButton.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButton.firstChild){
+        answerButton.removeChild(answerButton.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+}
+
+startQuiz();
